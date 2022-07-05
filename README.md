f2e42f67fde4211a27cb4eb37c379e779fd17501
Antoni Bergas Galmes <abergas@apsl.net>
2022-07-05 13:42:28 +0200

    Readme update

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This repository contains the code after following the guide: https://tutorial.djangogirls.org/es/
 
 Django version: 3.2.10
+
+Available online here: (http://abergas.pythonanywhere.com/)[https://github.com/metabig/test.git]
--- pushGitLabGitHub.sh
+++ pushGitLabGitHub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git remote set-url origin git@github.com:metabig/test.git
 git add .
 git commit -m "$1"
+git remote set-url origin git@github.com:metabig/test.git
 git push
 git remote set-url origin git@gitlab.apsl.net:abergas/django-blog.git
+git push
