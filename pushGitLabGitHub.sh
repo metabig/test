c1039a94fd98025f93078f76326b4da608ddc1a0
Antoni Bergas Galmes <abergas@apsl.net>
2022-07-05 13:33:19 +0200

    Change multirepo script

--- pushGitLabGitHub.sh
+++ pushGitLabGitHub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git remote set-url origin git@github.com:metabig/test.git
 git add .
-git commit -m "Updates"
+git commit -m "$1"
 git push
 git remote set-url origin git@gitlab.apsl.net:abergas/django-blog.git
