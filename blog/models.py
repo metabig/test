b58611c1a14bd82d1dcd5855ffb1e3ca4dfe8fd9
Antoni Bergas Galmes <abergas@apsl.net>
2022-07-06 12:33:21 +0200

    Class Based Views

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -15,6 +16,9 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+        
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.kwargs['pk'],})
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,43 +1,51 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.urls import reverse
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from django.views.generic import DetailView, CreateView
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page')
-    paginated_posts = paginator.get_page(page_number)
-    return render(request, 'blog/post_list.html', {'posts': paginated_posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class postList(ListView):
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 10
+    template_name = 'blog/post_list.html'
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+
+class postDetail(DetailView):
+    model = Post
+    #context_object_name: 'post'
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+class postCreate(CreateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ["title", "text"]
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.published_date = timezone.now()
+        self.object.save()
+        return HttpResponseRedirect(reverse('post_detail', kwargs={'pk': self.object.pk}))
+
+
+class postUpdate(UpdateView):
+    model = Post
+    template_name_suffix = '_edit'
+    fields = ["title", "text"]
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
