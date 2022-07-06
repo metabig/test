64030a3218ab26f798f5fa8647fded532e6815fb
Antoni Bergas Galmes <abergas@apsl.net>
2022-07-06 09:58:35 +0200

    [EJ1.0] Django academy: Blog

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    paginated_posts = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'posts': paginated_posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -22,4 +26,18 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
--- multiPush.sh
+++ multiPush.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git add --all .
-git commit -m "$1"
 git remote set-url origin git@github.com:metabig/test.git
 git push
 git remote set-url origin git@gitlab.apsl.net:abergas/django-blog.git
