f38fef3c96a5d74da083b2a9b544281f7fddb98a
Antoni Bergas Galmes <abergas@apsl.net>
2022-07-07 09:43:28 +0200

    [EJ 2.1] Stylish forms

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy form theme
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
